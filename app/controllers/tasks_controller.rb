54945a7df770600f333f686bc4d1204185079a35
Rishabh Singh <ris8645208@gmail.com>
2022-06-19 13:57:50 +0530

    chore: add current user and its associated data in controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:project_id])
+      @project = current_user.projects.find(params[:project_id])
     end
 
     def set_task
